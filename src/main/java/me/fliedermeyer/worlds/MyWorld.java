f1fd718d2186d874392fb9bab8042eacc32ce6ea
Fliedermeyer <154363302+Fliedermeyer@users.noreply.github.com>
2024-09-29 12:10:39 +0200

    Renaming

--- src/main/java/me/fliedermeyer/worlds/MyWorld.java
+++ src/main/java/me/fliedermeyer/worlds/MyWorld.java
@@ -1,21 +1,17 @@
-package me.leotuet.worlds;
+package me.fliedermeyer.worlds;
 
 // WARNING: This file is auto-generated and any changes to it will be overwritten
 import greenfoot.World;
-import me.leotuet.actors.Bean;
 
 /**
  * 
  */
 public class MyWorld extends World {
 
-	/**
-	 * Constructor for objects of class MyWorld.
-	 */
+	// Constructor for objects of class MyWorld.
+
 	public MyWorld() {
 		super(600, 400, 1);
 
-		var bean = new Bean();
-		addObject(bean, 300, 200);
 	}
 }

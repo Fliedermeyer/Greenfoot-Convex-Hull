b1379a2cde4b270f19257c8d1b352320d4c9e4e2
Fliedermeyer <154363302+Fliedermeyer@users.noreply.github.com>
2024-12-28 22:32:05 +0100

    Added average collision check duration calling a method

--- src/main/java/me/fliedermeyer/worlds/MyWorld.java
+++ src/main/java/me/fliedermeyer/worlds/MyWorld.java
@@ -1,12 +1,9 @@
 package me.fliedermeyer.worlds;
 
 import greenfoot.World;
-import me.fliedermeyer.actors.AABBAsteroid;
-import me.fliedermeyer.actors.AABBRocket;
+import me.fliedermeyer.actors.CHActor;
 import me.fliedermeyer.actors.CHAsteroid;
 import me.fliedermeyer.actors.CHRocket;
-import me.fliedermeyer.actors.OBBAsteroid;
-import javax.swing.JOptionPane;
 
 // Class for the game world
 public class MyWorld extends World {
@@ -14,27 +11,24 @@ public class MyWorld extends World {
 	// Constructor for objects of MyWorld class
 	public MyWorld() {
 		super(1000, 700, 1); // Sets the boundaries of the world
-	
-		String input = JOptionPane.showInputDialog(null, "Do you want to test axis aligned bounding box collision [1] or convex hull collision [2]?");
 
-		if (input.equals("1")){
-			addObject(new AABBRocket(), getWidth() / 2, getHeight() - 45);
+		int numTests = 100;
+		long totalTime = 0;
 
-			addObject(new AABBAsteroid(), getWidth() / 2, 0);
-			
-			addObject(new AABBAsteroid(), getWidth() / 2 + 300, 0);
-	
-			addObject(new OBBAsteroid(), getWidth() / 2 + 150, 0);
-		} else if (input.equals("2")){
-			addObject(new CHAsteroid(), getWidth() / 2 - 150, 0);
+		CHActor rocket = new CHRocket();
+		CHActor asteroid = new CHAsteroid();
 
-			addObject(new CHAsteroid(), getWidth() / 2 - 300, 0);
+		addObject(rocket, getWidth() / 2, getHeight() - 45);
+		addObject(asteroid, getWidth() / 2, 0);
 
-			addObject(new CHRocket(), getWidth() / 2, getHeight() - 45);
-		} else {
-			System.out.println("Unvalid input; Make sure you either type in 1 or 2");
+		for (int i = 0; i < numTests; i++) {
+			long startTime = System.nanoTime();
+			rocket.checkCollision(asteroid);
+			long endTime = System.nanoTime();
+			System.out.println("Single test duration calling a method (ns): " + (endTime - startTime));
+			totalTime += (endTime - startTime);
 		}
-		
 
+		System.out.println("Average collision check duration calling a method (ns): " + (totalTime / numTests));
 	}
 }
